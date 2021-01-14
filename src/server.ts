e507e0c92d66832c5133827cf3954b5b56eebc33
Abdullahi Yusuf <abdul.benyusuf@gmail.com>
2021-01-14 03:05:13 +0100

    completed TODOs

--- src/server.ts
+++ src/server.ts
@@ -28,6 +28,26 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+  app.get( "/filteredimage", async ( req: Request, res: Response ) => {
+
+    const { image_url } = req.query;
+
+    if ( !image_url ) {
+      return res.status(400)
+                .send(`image url is required`);
+    }
+
+    filterImageFromURL(image_url).then(async filteredpath => {
+      const files = [filteredpath]  
+      res.status(200).sendFile(filteredpath);
+      res.on('finish', async function() {
+        await deleteLocalFiles(files);
+      });
+    }).catch(err => {
+      return res.status(500).send(err.message);
+    });
+    
+  } );
 
   //! END @TODO1
   
